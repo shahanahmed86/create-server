a33ec3346a047f9e513445ab85b35b8ea0297941
Shahan <shahanahmed86@github.com>
2022-02-21 18:12:33 +0500

    fixes

--- template/source/db_deploy.sh
+++ template/source/db_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 printf "Please wait"
-while ! mysql --user=root --password=${DB_PASS} --host=${DB_HOST} -e "SELECT 1" >/dev/null 2>&1;
+while ! mysql --user=root --password="${DB_PASS}" --host="${DB_HOST}" -e "SELECT 1" >/dev/null 2>&1;
   do
     printf "."
     sleep 2
