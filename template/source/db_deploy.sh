300a38a39f03d01eba504d74cf4029cead1568f5
Shahan <shahanahmed86@github.com>
2022-04-11 03:26:15 +0500

    fixes

--- template/source/db_deploy.sh
+++ template/source/db_deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-while ! mysql --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" -e "SELECT 1" >/dev/null 2>&1;
-  do
-    printf "."
-    sleep 2
-  done
-  echo
+while ! mysql --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" -e "SELECT 1" >/dev/null 2>&1; do
+  printf "."
+  sleep 2
+done
+echo
+
 mysql --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" "${DB_NAME}" -e "DESC User" >/dev/null 2>&1
-if [ $? != 0 ]
-  then npm run db:deploy
-  else npm run db:generate
+if [ $? != 0 ]; then
+  npm run db:deploy
+else
+  npm run db:generate
 fi
