518ca75bd59fb68621017852a6d5e7811c55ddb9
Shahan <shahan.khaan@gmail.com>
2022-06-23 19:47:00 +0500

    fixes

--- template/source/healthchecks/server-healthcheck.js
+++ template/source/healthchecks/server-healthcheck.js
@@ -5,7 +5,7 @@ var options = {
 	timeout: 2000,
 	host: 'localhost',
 	port: process.env.PORT || 4000,
-	path: '/healthcheck', // must be the same as HEALTHCHECK in Dockerfile
+	path: '/api/healthcheck', // must be the same as HEALTHCHECK in Dockerfile
 };
 
 var request = http.request(options, (res) => {
